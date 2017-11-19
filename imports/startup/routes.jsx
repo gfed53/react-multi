55ab1aa979f631f0137dcd353b4bdff47a0b6e43
gfed53 <gfederico7153@gmail.com>
2017-11-19 16:42:17 -0500

    Initial commit. Basic project structure.

---
+++ imports/startup/routes.jsx
@@ -0,0 +1,11 @@
+import { FlowRouter } from 'meteor/kadira:flow-router';
+import React from 'react';
+import { mount } from 'react-mounter';
+
+// All Layouts and Pages
+import { App } from '../ui/App.jsx';
+import Home from '../ui/Home.jsx';
+import Page from '../ui/Page.jsx';
+
+
+
