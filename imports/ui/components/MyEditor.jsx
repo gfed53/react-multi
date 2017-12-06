3b24e1ab94a1511290d892fe31785ca7c028b5d5
gfed53 <gfederico7153@gmail.com>
2017-12-06 16:32:23 -0500

    Added draft-js-utils dep, added some test code.

--- imports/ui/components/MyEditor.jsx
+++ imports/ui/components/MyEditor.jsx
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils } from 'draft-js';
+import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
 
 export default class MyEditor extends Component {
 
   constructor(props) {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
+
     this.onChange = (editorState) => {
-      console.log('editorState',editorState);
+      // console.log('editorState',editorState);
       this.setState({editorState});
     };
+
+    this.logState = () => {
+      const content = this.state.editorState.getCurrentContent();
+      console.log(convertToRaw(content));
+    };
+
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
   }
 
@@ -22,15 +29,57 @@ export default class MyEditor extends Component {
     return 'not-handled';
   }
 
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+
+  _onTestClick() {
+    console.log('currentContent',this.state.editorState.getCurrentContent());
+  }
+
+  _onTestLinkClick() {
+    // console.log('currentContent',this.state.editorState.getCurrentContent());
+    const contentState = this.state.editorState.getCurrentContent();
+
+    const contentStateWithEntity = contentState.createEntity(
+        'LINK',
+        'MUTABLE',
+        {url: 'http://www.gregoryfederico.com'}
+      );
+
+    // console.log()
+
+    // const newEditorState = EditorState.set(editorState, { currentContent: contentStateWithEntity });
+
+    // this.setState({
+    //   editorState: RichUtils.toggleLink(
+    //     newEditorState,
+    //     newEditorState.getSelection(),
+    //     entityKey
+    //   ),
+    //   showURLInput: false,
+    //   urlValue: '',
+    // }, () => {
+    //   setTimeout(() => this.refs.editor.focus(), 0);
+    // });
+
+
+  }
+
 
 
   render() {
     return (
+      <div>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <button onClick={this._onTestClick.bind(this)}>Test</button>
+        <button onClick={this.logState}>Log State</button>
         <Editor 
           editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
-           />
+        />
+      </div>
     );
   }
 }
