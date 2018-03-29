d4ab4ea3cc1309bb1eb56610cb446723a16d9961
gfed53 <gfederico7153@gmail.com>
2018-03-29 17:22:11 -0400

    Added linkify dependency along with button to toggle readOnly condition of Editor component.

--- imports/ui/components/MyEditor.jsx
+++ imports/ui/components/MyEditor.jsx
@@ -1,5 +1,12 @@
 import React, { Component } from 'react';
-import { Editor, EditorState, RichUtils, convertToRaw } from 'draft-js';
+import { EditorState, RichUtils, convertToRaw } from 'draft-js';
+import Editor from 'draft-js-plugins-editor';
+import createLinkifyPlugin from 'draft-js-linkify-plugin';
+
+const linkifyPlugin = createLinkifyPlugin({
+  target: '_blank'
+}
+);
 
 // testing
 import Text from './Text';
@@ -8,7 +15,10 @@ export default class MyEditor extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+    this.state = {
+      editorState: EditorState.createEmpty(),
+      readOnly: false
+    };
 
     this.onChange = (editorState) => {
       // console.log('editorState',editorState);
@@ -21,6 +31,7 @@ export default class MyEditor extends Component {
     };
 
     this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.onReadToggleClick = this._onReadToggleClick.bind(this);
   }
 
   handleKeyCommand(command, editorState) {
@@ -38,6 +49,20 @@ export default class MyEditor extends Component {
 
   _onTestClick() {
     console.log('currentContent',this.state.editorState.getCurrentContent());
+
+  }
+
+  _onReadToggleClick() {
+    console.log('currentContent',this.state.editorState.getCurrentContent());
+
+    this.setState((prevState, props) => {
+      return {
+        readOnly: !prevState.readOnly
+      }
+    })
+
+    setTimeout(() => { console.log('this.state',this.state); }, 0);
+
   }
 
 
@@ -48,10 +73,13 @@ export default class MyEditor extends Component {
         <button onClick={this._onBoldClick.bind(this)}>Bold</button>
         <button onClick={this._onTestClick.bind(this)}>Test</button>
         <button onClick={this.logState}>Log State</button>
+        <button onClick={this.onReadToggleClick}>Toggle Read</button>
         <Editor 
           editorState={this.state.editorState}
           handleKeyCommand={this.handleKeyCommand}
           onChange={this.onChange}
+          plugins={[linkifyPlugin]}
+          readOnly={this.state.readOnly}
         />
         <h3>HTML output</h3>
         <Text data={this.state.editorState} />
